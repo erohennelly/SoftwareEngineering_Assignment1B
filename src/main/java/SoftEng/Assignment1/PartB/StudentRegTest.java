008f8cd87fd00a313048330dcb8c2dd332051121
Eoghan Hennelly <erohennelly@hotmail.com>
2018-10-31 15:53:48 +0000

    [New Changes]

--- src/main/java/SoftEng/Assignment1/PartB/StudentRegTest.java
+++ src/main/java/SoftEng/Assignment1/PartB/StudentRegTest.java
@@ -1,61 +1,66 @@
 package SoftEng.Assignment1.PartB;
 
-import java.util.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.joda.time.DateTime;
+
 
 import SoftEng.Assignment1.mavenproject1.CourseProgramme;
 import SoftEng.Assignment1.mavenproject1.Module;
 import SoftEng.Assignment1.mavenproject1.Student;
 
+// @Eoghan Hennelly
+
 public class StudentRegTest {
 	
-	public static void main(String args[]) throws ParseException {
+	
+
+public static void main(String args[]) throws ParseException {
 		
 		SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Date date1 = dateformat.parse("12/12/1994");
-		Student valarie = new Student("Valarie", date1, 1001);
+		Student lilly = new Student("Lilly", date1, 4352);
 		
 		Date date2 = dateformat.parse("13/3/1996");
-		Student david = new Student("David", date2, 1002);
+		Student david = new Student("David", date2, 4351);
 		
 		Date date3 = dateformat.parse("1/1/1994");
-		Student mary = new Student("Mary", date3, 1003);
+		Student michelle = new Student("Michelle", date3, 4353);
 		
 		Date date4 = dateformat.parse("25/9/1995");
-		Student john = new Student("John", date4, 1004);
+		Student johnny = new Student("Johnny", date4, 4354);
 		
 		Date date5 = dateformat.parse("8/8/1998");
-		Student elena = new Student("Elena", date5, 1005);
+		Student megan = new Student("Megan", date5, 4355);
 		
 		Date date6 = dateformat.parse("11/5/1995");
-		Student alan = new Student("Alan", date6, 1006);
+		Student alan = new Student("Alan", date6, 4356);
 		
 		
 		Student[] students1 = new Student[3];
-		students1[0] = valarie;
+		students1[0] = lilly;
 		students1[1] = david;
-		students1[2] = mary;
-		Module softEng = new Module("Software Engineering", "CT417", students1);
+		students1[2] = michelle;
+		Module softEng = new Module("Software Eng", "CT417", students1);
 		
 		Student[] students2 = new Student[3];
-		students2[0] = valarie;
+		students2[0] = lilly;
 		students2[1] = david;
-		students2[2] = mary;
+		students2[2] = michelle;
 		Module dataSys = new Module("Database Systems", "CT230", students2);
 		
 		Student[] students3 = new Student[3];
-		students3[0] = john;
-		students3[1] = elena;
+		students3[0] = johnny;
+		students3[1] = megan;
 		students3[2] = alan;
-		Module teleCom = new Module("Telecommunications", "EE453", students3);
+		Module proSkil = new Module("Pro Skills", "CT404", students3);
 		
 		Student[] students4 = new Student[3];
-		students4[0] = john;
-		students4[1] = elena;
+		students4[0] = johnny;
+		students4[1] = megan;
 		students4[2] = alan;
 		Module realTim = new Module("Real Time Systems", "EE420", students4);
 		
@@ -68,7 +73,7 @@ public class StudentRegTest {
 		CourseProgramme comSci = new CourseProgramme("Computer Science", modules1, start1, end1);
 		
 		Module[] modules2 = new Module[2];
-		modules2[0] = teleCom;
+		modules2[0] = proSkil;
 		modules2[1] = realTim;
 		DateTime start2 = new DateTime(25/8/2016);
 		DateTime end2 = new DateTime(9/5/2020);
